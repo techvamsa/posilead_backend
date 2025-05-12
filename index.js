1708f417461541ccc302e037a1ac3143e47eb5db
ravi gupta <ravibaba2022@gmail.com>
2025-05-12 23:56:25 +0530

    add build serve file

--- index.js
+++ index.js
@@ -71,6 +71,10 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
